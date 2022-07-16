bf1b8ac44613995a21f18a9747c35a9eaa8c9855
amey bhattad <amey.bhattad72@gmail.com>
2022-07-16 17:55:01 +0530

    Added keys

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose");
 
-API_key = pk.eyJ1IjoiYW1leTQ5NyIsImEiOiJja2htMDQzc20wYW9jMndxcXJ6OTllMGo1In0.FGiBqt1XvZ6b4yZA76caQ=====
+apikey = pkeyJ1IjoiYW1leTQ5NyIsImEiOiJja2htMDQzc20wYW9jMndxcXJ6OTllMGo1In0FGiBqt1XvZ6b4yZA76caQ=====
 
